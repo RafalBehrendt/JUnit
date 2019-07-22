cee1600126ddc2743f7f3153f31f33c6ff20ef16
wang <rav5738@gmail.com>
2019-07-22 23:35:49 +0200

    JUnit tests Using Junit 4

---
+++ test/com/junit/AssertTest.java
@@ -0,0 +1,15 @@
+package com.junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AssertTest {
+
+    @Test
+    public void test(){
+
+    }
+
+  }
